a87d867ba4190c9544f4e09447f838ffdb3336a0
mrmolaei <mrmolaei.ir@gmail.com>
2019-03-16 13:41:37 +0330

    Add: Category Page

---
+++ category.php
@@ -0,0 +1,41 @@
+<?php
+get_header();
+?>
+
+	<section class="blog category">
+
+		<?php
+		the_archive_title('<h1>' , '</h1>');
+		?>
+
+		<div class="list">
+			<?php if ( have_posts() ) : ?><?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					/*
+					 * Include the Post-Type-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_type() );
+
+				endwhile;
+
+				the_posts_pagination( array(
+					'mid_size'  => 2,
+					'prev_text' => __( 'Previous' ),
+					'next_text' => __( 'Next' ),
+				) );
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
+			?>
+		</div>
+	</section>
+<?php
+get_footer();
