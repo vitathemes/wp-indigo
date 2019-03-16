fc95a0b41f6e6857161b36c0e939e29471d5cc9f
mrmolaei <mrmolaei.ir@gmail.com>
2019-03-16 14:11:35 +0330

    Update: Social Fields Placeholder

--- inc/customizer.php
+++ inc/customizer.php
@@ -43,11 +43,9 @@ function indigo_customizer_settings( $wp_customize ) {
 	) );
 	$wp_customize->add_setting( 'social-mail' , array(
 		'transport'   => 'refresh',
-		'default'       => 'https://mail.com',
 	) );
 	$wp_customize->add_setting( 'social-facebook' , array(
 		'transport'   => 'refresh',
-		'default'       => 'https://facebook.com',
 	) );
 	$wp_customize->add_setting( 'social-twitter' , array(
 		'transport'   => 'refresh',
@@ -100,7 +98,7 @@ function indigo_customizer_settings( $wp_customize ) {
 		'type'        => 'url',
 		'section'    => 'social-section',
 		'input_attrs' => array(
-			'placeholder' => __( 'mailto:youremail@domain.com', 'indigo' ),
+			'placeholder' => __( 'mailto:youremail@example.com', 'indigo' ),
 		)
 	) ) );
 
@@ -108,96 +106,144 @@ function indigo_customizer_settings( $wp_customize ) {
 		'label'        => 'Facebook',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.facebook.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-twitter', array(
 		'label'        => 'Twitter',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://twitter.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-instagram', array(
 		'label'        => 'Instagram',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.instagram.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-pinterest', array(
 		'label'        => 'Pinterest',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.pinterest.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-linkedin', array(
 		'label'        => 'Linkedin',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.linkedin.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-youtube', array(
 		'label'        => 'Youtube',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.youtube.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-spotify', array(
 		'label'        => 'Spotify',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.spotify.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-github', array(
 		'label'        => 'Github',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.github.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-gitlab', array(
 		'label'        => 'Gitlab',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.gitlab.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-lastfm', array(
 		'label'        => 'Lastfm',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.lastfm.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-stackoverflow', array(
 		'label'        => 'Stackoverflow',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.stackoverflow.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-quora', array(
 		'label'        => 'Quora',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.quora.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-reddit', array(
 		'label'        => 'Reddit',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.reddit.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-medium', array(
 		'label'        => 'Medium',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.medium.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-vimeo', array(
 		'label'        => 'Vimeo',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.vimeo.com/example', 'indigo' ),
+		)
 	) ) );
 
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-lanyrd', array(
 		'label'        => 'Lanyrd',
 		'type'        => 'url',
 		'section'    => 'social-section',
+		'input_attrs' => array(
+			'placeholder' => __( 'https://www.lanyrd.com/example', 'indigo' ),
+		)
 	) ) );
 	// Social Networks section
 }
