a55e9ad18e452d998575c969f049ff2e8afdf6fd
mrmolaei <mrmolaei.ir@gmail.com>
2019-03-17 09:22:40 +0330

    Fix: Comment Deep and navigation & Structure

--- comments.php
+++ comments.php
@@ -1,23 +1,61 @@
 <?php
 $default_avatar = 'http://zacvineyard.com/blog/wp-content/themes/zac/images/default-avatar.png';
 ?>
-	<a id="comments"></a>
-	<h2>Comments</h2>
-<?php if($comments) : ?>
-	<ol class="comments">
-		<?php foreach($comments as $comment) : ?>
-			<li id="comment-<?php comment_ID(); ?>" class="comment <?php if ($comment->user_id == 1) echo "authcomment";?>">
-				<?php if ($comment->comment_approved == '0') : ?>
-					<p>Your comment is awaiting approval</p>
-				<?php endif; ?>
-				<?php echo get_avatar(get_comment_author_email(), 48, $default_avatar); ?>
-				<div class="comment-info"><h3 class="comment-author"><?php comment_author_link(); ?></h3> on <small class="comment-date"><?php comment_date(); ?></small></div>
-				<?php comment_text(); ?>
-			</li>
-		<?php endforeach; ?>
-	</ol>
-<?php endif; ?>
+<?php
+/**
+ * The template for displaying Comments.
+ *
+ * The area of the page that contains comments and the comment form.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
 
+/*
+ * If the current post is protected by a password and the visitor has not yet
+ * entered the password we will return early without loading the comments.
+ */
+if ( post_password_required() )
+    return;
+?>
+
+<div id="comments" class="comments-area">
+
+    <?php if ( have_comments() ) : ?>
+        <h2 class="comments-title">
+            <?php
+                printf( _nx( 'One thought on "%2$s"', '%1$s thoughts on "%2$s"', get_comments_number(), 'comments title', 'twentythirteen' ),
+                    number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+            ?>
+        </h2>
+
+        <ol class="comment-list comments">
+            <?php
+                wp_list_comments( array(
+                    'style'       => 'ol',
+                    'short_ping'  => true,
+                    'avatar_size' => 64,
+                ) );
+            ?>
+        </ol><!-- .comment-list -->
+
+        <?php
+            // Are there comments to navigate through?
+            if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+        ?>
+        <nav class="navigation comment-navigation" role="navigation">
+            <h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'twentythirteen' ); ?></h1>
+            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentythirteen' ) ); ?></div>
+            <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentythirteen' ) ); ?></div>
+        </nav><!-- .comment-navigation -->
+        <?php endif; // Check for comment navigation ?>
+
+        <?php if ( ! comments_open() && get_comments_number() ) : ?>
+        <p class="no-comments"><?php _e( 'Comments are closed.' , 'twentythirteen' ); ?></p>
+        <?php endif; ?>
+
+    <?php endif; // have_comments() ?>
 <?php if(comments_open()) : ?>
 	<h2>Add Your Comment</h2>
 	<?php if(get_option('comment_registration') && !$user_ID) : ?>
