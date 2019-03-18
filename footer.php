c84e1dad10206d7424c0c43019ba7fd575f0bf02
mrmolaei <mrmolaei.ir@gmail.com>
2019-03-18 09:45:50 +0330

    Fix: Static Urls

--- footer.php
+++ footer.php
@@ -4,14 +4,14 @@
 	if ( get_theme_mod( 'name' ) != "" ) {
 		echo get_theme_mod( 'name' );
 	}
-	?> © <?php echo date( 'Y' ); ?> <a class="link" href="http://localhost:4000/feed.xml" target="_blank">
+	?> © <?php echo date( 'Y' ); ?> <a class="link" href="<?php echo site_url(); ?>/feed.xml" target="_blank">
         <svg class="icon icon-rss">
             <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/defs.svg/#icon-rss"></use>
         </svg>
     </a>
 
     <p class="extra">
-        <a target="_blank" class="link" href="https://github.com/sergiokopplin/indigo">Indigo theme</a> by
+        <a target="_blank" class="link" href="https://github.com/veronalabs/wp-indigo">WP-Indigo theme</a> by
         <a target="_blank" title="WordPress Solutions to Meet Your Needs and Help You Grow" class="link" href="https://veronalabs.com/">VeronaLabs</a>
     </p>
 </footer>
--- header.php
+++ header.php
@@ -6,11 +6,6 @@
     <meta http-equiv=X-UA-Compatible content="IE=edge,chrome=1">
     <meta name=viewport content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="icon" type="image/png" href="<?php echo $theme_url; ?>/assets/images/favicon/favicon-196x196.png" sizes="196x196"/>
-    <link rel="icon" type="image/png" href="http://localhost:4000/assets/images/favicon/favicon-96x96.png" sizes="96x96"/>
-    <link rel="icon" type="image/png" href="http://localhost:4000/assets/images/favicon/favicon-32x32.png" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="http://localhost:4000/assets/images/favicon/favicon-16x16.png" sizes="16x16"/>
-    <link rel="icon" type="image/png" href="http://localhost:4000/assets/images/favicon/favicon-128.png" sizes="128x128"/>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
