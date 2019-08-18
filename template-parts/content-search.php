2bd3bdee580d0f552990ce096fca800fe208f184
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2019-08-18 15:16:55 +0430

    Remove Featured image from archive & blog page

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -1,9 +1,4 @@
 <div class="item">
-	<?php if ( has_post_thumbnail() ): ?>
-        <div class="item-image">
-			<?php the_post_thumbnail( 'large' ); ?>
-        </div>
-	<?php endif; ?>
     <a class="url" href="<?php the_permalink(); ?>">
         <aside class="date">
             <time datetime="<?php echo get_the_date( get_option( 'date_format' ) ); ?>"><?php echo get_the_date(); ?></time>
