925faeb5a757322003f452aea38d061854798f9f
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2019-08-19 13:34:29 +0430

    #14: Move Kirki Codes to Customizer file

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,215 +1,299 @@
 <?php
-//add_action( 'customize_register', 'indigo_customizer_settings' );
-//
-//function indigo_customizer_settings( $wp_customize ) {
-//	// Social Networks section
-//	$wp_customize->add_section( 'social-section', array(
-//		'title'    => 'Social Networks',
-//		'priority' => 31,
-//	) );
-//	$wp_customize->add_setting( 'social-mail', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-facebook', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-twitter', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-instagram', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-pinterest', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-linkedin', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-youtube', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-spotify', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-github', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-gitlab', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-lastfm', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-stackoverflow', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-quora', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-reddit', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-medium', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-vimeo', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_setting( 'social-lanyrd', array(
-//		'transport' => 'refresh',
-//	) );
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-mail', array(
-//		'label'       => 'Mail',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'mailto:youremail@example.com', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-facebook', array(
-//		'label'       => 'Facebook',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.facebook.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-twitter', array(
-//		'label'       => 'Twitter',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://twitter.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-instagram', array(
-//		'label'       => 'Instagram',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.instagram.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-pinterest', array(
-//		'label'       => 'Pinterest',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.pinterest.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-linkedin', array(
-//		'label'       => 'Linkedin',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.linkedin.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-youtube', array(
-//		'label'       => 'Youtube',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.youtube.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-spotify', array(
-//		'label'       => 'Spotify',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.spotify.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-github', array(
-//		'label'       => 'Github',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.github.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-gitlab', array(
-//		'label'       => 'Gitlab',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.gitlab.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-lastfm', array(
-//		'label'       => 'Lastfm',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.lastfm.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-stackoverflow', array(
-//		'label'       => 'Stackoverflow',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.stackoverflow.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-quora', array(
-//		'label'       => 'Quora',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.quora.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-reddit', array(
-//		'label'       => 'Reddit',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.reddit.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-medium', array(
-//		'label'       => 'Medium',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.medium.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-vimeo', array(
-//		'label'       => 'Vimeo',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.vimeo.com/example', 'indigo' ),
-//		)
-//	) ) );
-//
-//	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'social-lanyrd', array(
-//		'label'       => 'Lanyrd',
-//		'type'        => 'url',
-//		'section'     => 'social-section',
-//		'input_attrs' => array(
-//			'placeholder' => __( 'https://www.lanyrd.com/example', 'indigo' ),
-//		)
-//	) ) );
-//	// Social Networks section
-//}
 
+// Add config
+Kirki::add_config( 'indigo', array(
+	'option_type' => 'theme_mod'
+) );
+
+// Add sections
+Kirki::add_section( 'branding', array(
+	'title'    => esc_html__( 'Branding', 'indigo' ),
+	'panel'    => '',
+	'priority' => 3,
+) );
+
+Kirki::add_section( 'typography', array(
+	'title'    => esc_html__( 'Typography', 'indigo' ),
+	'panel'    => '',
+	'priority' => 4,
+) );
+
+Kirki::add_section( 'elements', array(
+	'title'    => esc_html__( 'Elements', 'indigo' ),
+	'panel'    => '',
+	'priority' => 5,
+) );
+
+Kirki::add_section( 'socials', array(
+	'title'    => esc_html__( 'Social Networks', 'indigo' ),
+	'panel'    => '',
+	'priority' => 6,
+) );
+
+// Add Branding fields
+
+// -- Branding Fields --
+Kirki::add_field( 'indigo', [
+	'type'      => 'background',
+	'settings'  => 'branding_background',
+	'label'     => esc_html__( 'Background', 'indigo' ),
+	'section'   => 'branding',
+	'default'   => [
+		'background-color'      => 'fff',
+		'background-image'      => '',
+		'background-repeat'     => 'repeat',
+		'background-position'   => 'center center',
+		'background-size'       => 'cover',
+		'background-attachment' => 'scroll',
+	],
+	'transport' => 'auto',
+	'output'    => [
+		[
+			'element' => 'body',
+		],
+	],
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'color',
+	'settings' => 'branding_primary_color',
+	'label'    => __( 'Primary Color', 'indigo' ),
+	'section'  => 'branding',
+	'default'  => '#3F51B5',
+] );
+
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'color',
+	'settings' => 'branding_secondary_color',
+	'label'    => __( 'Secondary Color', 'indigo' ),
+	'section'  => 'branding',
+	'default'  => '#A1A1A1',
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'color',
+	'settings' => 'branding_tertiary_color',
+	'label'    => __( 'Tertiary Color', 'indigo' ),
+	'section'  => 'branding',
+	'default'  => '#666666',
+] );
+
+
+// -- Typography Fields --
+
+
+// -- Typography Fields --
+
+
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'toggle',
+	'settings' => 'show_share_icons',
+	'label'    => esc_html__( 'Show Share Icons', 'indigo' ),
+	'section'  => 'elements',
+	'default'  => '1',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'toggle',
+	'settings' => 'show_post_thumbnail',
+	'label'    => esc_html__( 'Show Post Thumbnail', 'indigo' ),
+	'section'  => 'elements',
+	'default'  => '1',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'toggle',
+	'settings' => 'show_profile_section',
+	'label'    => esc_html__( 'show_profile_section', 'indigo' ),
+	'description'    => esc_html__( 'Show profile section in pages: About/Blog/Home', 'indigo' ),
+	'section'  => 'elements',
+	'default'  => '1',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'text',
+	'settings' => 'copyright_text',
+	'label'    => esc_html__( 'Copyright Text', 'indigo' ),
+	'section'  => 'elements',
+	'priority' => 10,
+] );
+
+
+Kirki::add_field( 'indigo', [
+	'type'        => 'code',
+	'settings'    => 'profile_animation',
+	'label'       => esc_html__( 'Profile Animation', 'indigo' ),
+	'description' => esc_html__( 'Animation for profile section.', 'indigo' ),
+	'section'     => 'elements',
+	'default'     => '',
+	'choices'     => [
+		'language' => 'css',
+	],
+] );
+
+
+// Social Networks Fields
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-mail',
+	'label'    => __( 'Email', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-facebook',
+	'label'    => __( 'Facebook', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-twitter',
+	'label'    => __( 'Twitter', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-instagram',
+	'label'    => __( 'Instagram', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-pinterest',
+	'label'    => __( 'Pinterest', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-linkedin',
+	'label'    => __( 'Linkedin', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-youtube',
+	'label'    => __( 'Youtube', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-spotify',
+	'label'    => __( 'Spotify', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-github',
+	'label'    => __( 'Github', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-gitlab',
+	'label'    => __( 'Gitlab', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-lastfm',
+	'label'    => __( 'Lastfm', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-stackoverflow',
+	'label'    => __( 'Stackoverflow', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-quora',
+	'label'    => __( 'Quora', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-reddit',
+	'label'    => __( 'Reddit', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-medium',
+	'label'    => __( 'Medium', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-vimeo',
+	'label'    => __( 'Vimeo', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'     => 'link',
+	'settings' => 'social-lanyrd',
+	'label'    => __( 'Lanyrd', 'indigo' ),
+	'section'  => 'socials',
+	'priority' => 10,
+] );
+
+Kirki::add_field( 'indigo', [
+	'type'        => 'typography',
+	'settings'    => 'typography',
+	'label'       => esc_html__( 'Control Label', 'indigo' ),
+	'section'     => 'typography',
+	'default'     => [
+		'font-family'    => 'Roboto',
+		'variant'        => 'regular',
+		'font-size'      => '14px',
+		'line-height'    => '1.5',
+		'letter-spacing' => '0',
+		'color'          => '#333333',
+		'text-transform' => 'none',
+		'text-align'     => 'left',
+	],
+	'priority'    => 10,
+	'transport'   => 'auto',
+	'output'      => [
+		[
+			'element' => 'body',
+		],
+	],
+] );
