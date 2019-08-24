dab4acab85d0d1b5c3f6c5a1615201756d005cd0
Navid Kashani <navid.kashani@gmail.com>
2019-08-24 12:09:59 +0430

    check condition for blog desc
    and remove unused echo

--- template-parts/profile.php
+++ template-parts/profile.php
@@ -3,8 +3,10 @@ if ( get_theme_mod( 'show_profile_section' , 1 ) ):
 ?>
 <header class="header-home animated">
     <div class="selfie <?php echo get_theme_mod('profile_animation' , true) ? "has-animation" : ''; ?>"><?php indigo_show_avatar(); ?></div>
-    <a class="title-link" href="<?php echo site_url(); ?>"><h1 class="title"><?php echo bloginfo( 'name' ); ?></h1></a>
-    <h2 class="description"><?php echo bloginfo( 'description' );; ?></h2>
+    <a class="title-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><h1 class="title"><?php bloginfo( 'name' ); ?></h1></a>
+	<?php if ( get_bloginfo( 'description' )  !== '' ) { ?>
+		<h2 class="description"><?php bloginfo( 'description' ); ?></h2>
+	<?php } ?>
     <div class="social-links">
 		<?php indigo_show_socials(); ?>
     </div>
