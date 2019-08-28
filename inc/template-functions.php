a7cb5f9809ecbaf0c98d7ff4014eb31ee437a973
Navid Kashani <navid.kashani@gmail.com>
2019-08-28 11:32:44 +0430

    add aria-label for email link in social links

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -45,7 +45,7 @@ function indigo_show_socials( $social_names ) {
 		if ( $social != "" ) {
 			$name = explode( '-', $social_name );
 			if (strpos($name[1], 'mail') !== false) {
-				echo '<a rel="noopener" class="link" data-title="' . $social . '" href="mailto:' . $social . '" target="_blank">
+				echo '<a aria-label="Email Me" rel="noopener" class="link" data-title="' . $social . '" href="mailto:' . $social . '" target="_blank">
 			<svg class="icon icon-facebook"><use xlink:href="' . get_bloginfo( 'template_url' ) . '/assets/images/defs.svg#icon-' . $name[1] . '"></use></svg>
 		</a>';
 			} else{
