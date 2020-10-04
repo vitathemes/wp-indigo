d352ea42b373e0354177a54b5e97a68ea07595ae
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2020-10-04 15:43:04 +0330

    Fix an escaping issue

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -12,10 +12,18 @@
     <div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-            <p><?php printf( wp_kses(
-					    /* translators: 1: link to WP admin new post page. */
-						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'wp-indigo' ), esc_url( admin_url( 'post-new.php' ) ) )
-				); ?></p>
+            <p><?php printf(
+		            '<p>' . wp_kses(
+		            /* translators: 1: link to WP admin new post page. */
+			            __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'wp-meliora' ),
+			            array(
+				            'a' => array(
+					            'href' => array(),
+				            ),
+			            )
+		            ) . '</p>',
+		            esc_url( admin_url( 'post-new.php' ) )
+	            ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
