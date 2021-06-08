b55c73495bc045a39977bc0a9ae57563ae53cbc0
nima <nimaprmdi@gmail.com>
2021-06-08 11:47:16 +0430

    Fix W3 validations

--- comments.php
+++ comments.php
@@ -30,11 +30,11 @@ if ( post_password_required() ) {
 		'fields' => array(
 						
 			// Author field
-			'author' => '<p class="comment-form-author h5 u-letter-space-small">'. esc_html__( 'Name*', 'wp-indigo' ).'<br /><input type="text" id="author" name="author" aria-required="true" required></input></p>',
+			'author' => '<p class="comment-form-author h5 u-letter-space-small">'. esc_html__( 'Name*', 'wp-indigo' ).'<br /><input type="text" id="author" name="author" aria-required="true" required /></p>',
 			// Email Field
-			'email' => '<p class="comment-form-email h5 u-letter-space-small">'. esc_html__( 'Email*', 'wp-indigo' ).'<br /><input type="email" id="email" name="email" required></input></p>',
+			'email' => '<p class="comment-form-email h5 u-letter-space-small">'. esc_html__( 'Email*', 'wp-indigo' ).'<br /><input type="email" id="email" name="email" required /></p>',
 			// URL Field
-			'url' => '<p class="comment-form-url h5 u-letter-space-small">'. esc_html__( 'Website', 'wp-indigo' ).'<br /><input type="url" id="url" name="url" required></input></p>',
+			'url' => '<p class="comment-form-url h5 u-letter-space-small">'. esc_html__( 'Website', 'wp-indigo' ).'<br /><input type="url" id="url" name="url" required /></p>',
 			// Cookies
 			'cookies' => '<div class="c-comment__cookie"><input type="checkbox" name="wp-comment-cookies-consent" required><span class="c-comments__cookie h5 u-letter-space-small">' . esc_html__(' Save my name, email, and website in this browser for the next time I comment', 'wp-indigo' ) .'</span></div>',
 			
@@ -42,11 +42,11 @@ if ( post_password_required() ) {
 		// Change the title of send button
 		'label_submit' => esc_html__( 'POST COMMENT', 'wp-indigo'),
 		// Change the title of the reply section
-		'title_reply' => '<p class="c-comments__title c-comments__title--primary h2">'. esc_html__( 'Join the Conversation' , 'wp-indigo') .'</p><p class="c-comments__title h3 u-letter-space-medium">'. esc_html__( 'Leave a reply' , 'wp-indigo') .'</p>',
+		'title_reply' => '<span class="c-comments__title c-comments__title--primary h2">'. esc_html__( 'Join the Conversation' , 'wp-indigo') .'</span><span class="c-comments__title c-comments__title--reply h3 u-letter-space-medium">'. esc_html__( 'Leave a reply' , 'wp-indigo') .'</span>',
 		// Change the title of the reply section
 		'title_reply_to' =>   esc_html__( 'Reply' , 'wp-indigo'),
 		//Cancel Reply Text
-		'cancel_reply_link' =>	'<p class="h4 u-link--secondary">'.esc_html__( 'Cancel Reply', 'wp-indigo' ).'</p>',
+		'cancel_reply_link' =>	'<span class="h4 u-link--secondary">'.esc_html__( 'Cancel Reply', 'wp-indigo' ).'</span>',
 		// Redefine your own textarea (the comment body).
 		'comment_field' => '<p class="comment-form-comment h5 u-letter-space-small">'. esc_html__( 'Comment*', 'wp-indigo' ).'<br /><textarea id="comment" name="comment" aria-required="true" ></textarea></p>',
 		//Message Before Comment
