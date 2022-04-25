ae429ce8dd3dc9de9e7b746bbed29ff08cf4bc5f
Nima Pourmohamadi <nimaprmdi@gmail.com>
2022-04-25 11:14:23 +0430

    Fix minor style issues

--- searchform.php
+++ searchform.php
@@ -6,7 +6,7 @@
  */
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url( '/' )); ?>">
-    <label>
+    <label class="search-label">
         <span class="screen-reader-text"><?php echo esc_html_e( 'Search for:', 'wp-indigo' ) ?></span>
         <input type="search" class="search-field" placeholder="<?php echo esc_attr__( 'Search…', 'wp-indigo' ) ?>" value="<?php echo get_search_query() ?>" name="s"
             title="<?php echo esc_attr__( 'Search for:', 'wp-indigo' ) ?>" />
